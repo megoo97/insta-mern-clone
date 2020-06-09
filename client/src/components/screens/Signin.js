86c7c4d25fb2200e7cbdf5af9c6fcb85dc5356a4
megoo <a.abdelmaged@entrenchtech.com>
2020-06-09 23:03:44 +0200

    add create post with auth and integrate with cloud

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React,{useState} from 'react';
+import {Link,useHistory} from 'react-router-dom';
+import M from'materialize-css';
 
 const Signin = () => {
+  const history =useHistory();
+  const [email,setEmail] =useState("");
+  const [password,setPassword] =useState("");
+  const postData = () => {
+    if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+      M.toast({html: "invalid email address" , classes:"#c62828 red darken-3"}) 
+      return;
+    }
+    fetch("/signin",{
+      method:"post",
+      headers:{
+        "content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        email,
+        password
+      })
+    }).then(res => res.json()).then(data =>{
+      if(data.error){  M.toast({html: data.error,classes:"#c62828 red darken-3"})}
+      else {
+        localStorage.setItem("jwt",data.token);
+        localStorage.setItem("user",JSON.stringify(data.user));
+        M.toast({html: "Signedin successed",classes:"#43a047 green darken-1"})
+        history.push('/')
+      }
+    }).catch(error =>{
+      console.log(error);
+    })
+  }
   return (
     <div className="my-card">
       <div className="card auth-card input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password"  />
-        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Signin</button>
+        <input type="text" placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        <input type="password" placeholder="password"  value={password} onChange={(e)=> setPassword(e.target.value)}/>
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" onClick={() => postData()}>Signin</button>
         <h5><Link to="/signup">Don't have an account?</Link></h5>
       </div>
     </div>
